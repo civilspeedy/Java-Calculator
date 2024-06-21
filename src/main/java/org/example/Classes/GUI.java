10a2a39a38e59ecf08a551ae37064b557415b83b
Charlie <106312897+civilspeedy@users.noreply.github.com>
2024-06-21 14:56:38 +0100

    display up

--- src/main/java/org/example/Classes/GUI.java
+++ src/main/java/org/example/Classes/GUI.java
@@ -12,30 +12,46 @@ import java.util.List;
 public class GUI {
     static JFrame mainFrame = new JFrame();
     static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    JPanel panel = new JPanel();
+    JLabel display = new JLabel();
+
+    static int value;
+    static int valueTwo;
+    static String operator;
 
     /**
      * The Constructor which sets the dimensions of the main frame.
      */
     public GUI() {
+        value = 0;
+        valueTwo = 0;
     }
 
     /**
      * Builds the mainframe and inserts the elements that will comprise the interface.
      */
     public void newWindow () {
+        panel.add(display);
+        display.setText("2");
+
+        panel.setBounds(0, 0, 250, 250);
+        panel.setBackground(Color.blue);
+
+        mainFrame.add(panel);
         mainFrame.setLayout(new GridLayout(4, 5));
         mainFrame.setSize(scaleSize());
+
         generateButtons();
+
         mainFrame.setResizable(false);
         mainFrame.setVisible(true);
-        System.out.println(mainFrame.getSize());
     }
 
     /**
      * Calculates the size of the window based of screen size.
      * @return the size of the window as a Dimension.
      */
-    public static Dimension scaleSize (){
+    public Dimension scaleSize (){
         double screenWidth = screenSize.getWidth();
         double screenHeight = screenSize.getHeight();
         int scaleFactor = 3;
@@ -49,10 +65,14 @@ public class GUI {
         return new Dimension(width, height);
     }
 
+    public void updateDisplay(String item){
+        display.setText(item);
+    }
+
     /**
      * Creates a List of buttons to be used in the inputPanel.
      */
-    public static void generateButtons(){
+    public void generateButtons(){
         List<String> symbols = new ArrayList<>();
 
         for (int i = 0; i <= 9; i++){
@@ -68,6 +88,25 @@ public class GUI {
                 public void actionPerformed(ActionEvent e) {
                     String action = e.getActionCommand();
                     System.out.println("This button was pressed " + action);
+                    try {
+                        // needs to be set up so that only int can come through
+                        updateDisplay(action);
+
+
+                    } catch (Exception err){
+                        switch (action) {
+                            case "+":
+                                break;
+                            case "-":
+                                break;
+                            case "÷":
+                                break;
+                            case ".":
+                                break;
+                            case "=":
+                                break;
+                        }
+                    }
                 }
             });
             mainFrame.add(button);
