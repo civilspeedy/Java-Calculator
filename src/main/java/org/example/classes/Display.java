51c250b30c88f98aa493e72b64a915fcb4769bb0
Charlie <106312897+civilspeedy@users.noreply.github.com>
2024-06-22 14:01:43 +0100

    working on array to string conversion

--- src/main/java/org/example/classes/Display.java
+++ src/main/java/org/example/classes/Display.java
@@ -1,33 +1,28 @@
 package org.example.classes;
 
 import javax.swing.JLabel;
+import java.util.ArrayList;
 
 public class Display {
     float value;
-    String stringValue;
+    ArrayList<Integer> displayArray;
     JLabel valueLabel;
 
     public Display(){
-        valueLabel = new JLabel("0.0");
+        valueLabel = new JLabel("0");
         value = 0;
-        stringValue = "0";
+        displayArray = new ArrayList<>();
     }
 
-    public void updateDisplayValue(float newValue){
-        String valueAsString = String.valueOf(newValue);
-        value = newValue;
-        stringValue = valueAsString;
-        valueLabel.setText(stringValue);
+    public void pushNumber(int number){
+        displayArray.add(number);
     }
 
-    public void updateDisplayValue(String newValue){
-        value = Float.valueOf(newValue);
-        stringValue = newValue;
-        valueLabel.setText(stringValue);
-
-    }
-
-    public JLabel getLabel (){
-        return valueLabel;
+    public String arrayToString(ArrayList<Integer> array){
+        StringBuilder string = new StringBuilder();
+        for (int item: array){
+            string.append(item);
+        }
+        return string.toString();
     }
 }
